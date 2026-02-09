8fb9488f1a41737fbd61ad048d1baafddc73ddf7
EduardoCruzRomero <educrurom@alum.us.es>
2026-02-09 23:04:14 +0100

    fix: Limpiar codigo

--- main.cpp
+++ main.cpp
@@ -6,29 +6,37 @@
 
 int main(int argc, char** argv) {
     
-    // 1. Contenedores de nubes de puntos
+
+
+    // ---- 1. Contenedores de nubes de puntos ----
     pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
     pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZI>);
 
-    // 2. Carga del archivo .pcd
+
+
+    // ---- 2. Carga del archivo .pcd ----
     if (pcl::io::loadPCDFile<pcl::PointXYZI>("../saved_pointcloud.pcd", *cloud) == -1) {
         std::cerr << "Error: No se ha podido cargar el archivo .pcd" << std::endl;
         return (-1);
     }
 
-    // 3. Configuración de RANSAC para detección de planos
+
+
+    // ---- 3. Configuración de RANSAC para detección de planos ----
     pcl::SACSegmentation<pcl::PointXYZI> seg;
     pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
     pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
 
     seg.setOptimizeCoefficients(true);      
-    seg.setModelType(pcl::SACMODEL_PLANE);  // Buscamos una geometría plana
-    seg.setMethodType(pcl::SAC_RANSAC);     // Algoritmo contra ruido (outliers)
+    seg.setModelType(pcl::SACMODEL_PLANE);      // Buscamos una geometría plana
+    seg.setMethodType(pcl::SAC_RANSAC);         // Algoritmo contra ruido (outliers)
     
-    seg.setMaxIterations(2000);    // Establecemos intentos para encontrar el plano
-    seg.setDistanceThreshold(0.5); // Establecemos umbral de 0,5
+    seg.setMaxIterations(2000);                 // Establecemos intentos para encontrar el plano
+    seg.setDistanceThreshold(0.5);              // Establecemos umbral de 0,5
 
-    // 4. Segmentación del suelo
+
+
+    // ---- 4. Segmentación del suelo ----
     seg.setInputCloud(cloud);
     seg.segment(*inliers, *coefficients);
 
@@ -37,14 +45,18 @@ int main(int argc, char** argv) {
         return (-1);
     }
 
-    // 5. Extracción de los puntos (Borrar el suelo)
+
+
+    // ---- 5. Extracción de los puntos (Borrar el suelo) ----
     pcl::ExtractIndices<pcl::PointXYZI> extract;
     extract.setInputCloud(cloud);
     extract.setIndices(inliers);
     extract.setNegative(true);  
     extract.filter(*cloud_filtered);
 
-    // 6. Guardado del resultado
+
+
+    // ---- 6. Guardado del resultado ----
     pcl::io::savePCDFileASCII("conos_aislados.pcd", *cloud_filtered);
     
     return (0);
