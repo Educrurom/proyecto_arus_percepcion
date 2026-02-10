aae8465d18afbf63484ac8f4fd22d7feeed352a6
EduardoCruzRomero <educrurom@alum.us.es>
2026-02-10 13:09:22 +0100

    fix: Retoques en codigo y documentación

--- README.md
+++ README.md
@@ -13,12 +13,15 @@ Para lograrlo, debemos localizar tres puntos (X, Y, Z) incluidos en ese plano. L
 Por otro lado, nos encontramos con el problema del ruido y los baches. Para solucionarlo, introduciremos un umbral (threshold).
 
 ### Optimización del algoritmo
-Además, podemos optimizar este algoritmo gracias al dato del ring:
+Además, podemos optimizar este algoritmo gracias al dato del ring y la intensidad con la que se devuelve el punto:
 * Podemos hacer uso de los rings, debido a que los de menor índice siempre impactan en el suelo. Por lo tanto, para no tener que iterar entre todos los puntos, restringiremos la búsqueda de los puntos entre estos rings.
+* Podemos hacer uso de la intensidad con la que se devuelve el punto, debido a que el suelo devolverá menos intensidad que el plastico de los conos. Por lo tanto, restringiremos la búsqueda de los puntos entre los de menor intensidad.
 
 ### RANSAC
 Para no reinventar la rueda, utilizaremos la librería PCL (Point Cloud Library), que incluye RANSAC, un algoritmo iterativo utilizado para estimar los parámetros de un modelo matemático, permitiéndonos centrar nuestros esfuerzos en la lógica de filtrado y optimización de los parámetros
 
+### Filtrado final
+Una vez obtenido el plano del suelo, se realizará el filtrado final utilizando tanto la distancia de los puntos al plano como la intensidad reflejada. Los puntos que se encuentren fuera del umbral del plano y su intensidad sea superior a la media de la intensidad del suelo, será tratado como un objeto, por lo que se mantendrán en la nube de puntos resultante.
 
 ---
 
@@ -37,4 +40,4 @@ La implementación se ha realizado en **C++** utilizando **Ubuntu 22.04**.
 mkdir build && cd build
 cmake ..
 make
-./eliminar_suelo
+./eliminar_suelo ../saved_pointcloud.pcd
--- main.cpp
+++ main.cpp
@@ -1,63 +1,103 @@
-#include <iostream>
+#define PCL_NO_PRECOMPILE
+
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/filters/extract_indices.h>
+#include <iostream>
+#include <cmath>
 
-int main(int argc, char** argv) {
-    
+// Estructura
+struct PointXYZIRT
+{
+  PCL_ADD_POINT4D;
+  float intensity;
+  uint16_t ring;
+  double timestamp;
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+} EIGEN_ALIGN16;
 
+POINT_CLOUD_REGISTER_POINT_STRUCT(
+    PointXYZIRT,
+    (float, x, x)
+    (float, y, y)
+    (float, z, z)
+    (float, intensity, intensity)
+    (uint16_t, ring, ring)
+    (double, timestamp, timestamp)
+)
 
-    // ---- 1. Contenedores de nubes de puntos ----
-    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
-    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZI>);
-
-
-
-    // ---- 2. Carga del archivo .pcd ----
-    if (pcl::io::loadPCDFile<pcl::PointXYZI>("../saved_pointcloud.pcd", *cloud) == -1) {
-        std::cerr << "Error: No se ha podido cargar el archivo .pcd" << std::endl;
-        return (-1);
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        std::cerr << "Uso: ./eliminar_suelo input.pcd" << std::endl;
+        return -1;
     }
 
+    // Cargar nube
+    pcl::PointCloud<PointXYZIRT>::Ptr cloud(new pcl::PointCloud<PointXYZIRT>);
+    if (pcl::io::loadPCDFile(argv[1], *cloud) == -1)
+    {
+        std::cerr << "Error al leer el archivo PCD" << std::endl;
+        return -1;
+    }
 
+    // Subnube optimizada
+    pcl::PointCloud<PointXYZIRT>::Ptr cloud_rings(new pcl::PointCloud<PointXYZIRT>);
+    float intensidad_max = 40.0f;
+    uint16_t umbral_ring_inferiores = 10;
 
-    // ---- 3. Configuración de RANSAC para detección de planos ----
-    pcl::SACSegmentation<pcl::PointXYZI> seg;
+    for (const auto& p : cloud->points)
+    {
+        if (p.ring <= umbral_ring_inferiores and p.intensity< intensidad_max) 
+            cloud_rings->push_back(p);
+    }
+
+    // RANSAC
+    pcl::SACSegmentation<PointXYZIRT> seg;
     pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
     pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
 
-    seg.setOptimizeCoefficients(true);      
-    seg.setModelType(pcl::SACMODEL_PLANE);      // Buscamos una geometría plana
-    seg.setMethodType(pcl::SAC_RANSAC);         // Algoritmo contra ruido (outliers)
-    
-    seg.setMaxIterations(2000);                 // Establecemos intentos para encontrar el plano
-    seg.setDistanceThreshold(0.5);              // Establecemos umbral de 0,5
-
-
-
-    // ---- 4. Segmentación del suelo ----
-    seg.setInputCloud(cloud);
+    seg.setOptimizeCoefficients(true);
+    seg.setModelType(pcl::SACMODEL_PLANE); 
+    seg.setMethodType(pcl::SAC_RANSAC);
+    seg.setDistanceThreshold(0.2);
+    seg.setInputCloud(cloud_rings);
     seg.segment(*inliers, *coefficients);
 
-    if (inliers->indices.size() == 0) {
-        std::cerr << "Error: No se encontró un plano de suelo en la escena." << std::endl;
-        return (-1);
+    if (inliers->indices.empty())
+    {
+        std::cerr << "No se pudo encontrar un plano del suelo" << std::endl;
+        return -1;
     }
 
-
-
-    // ---- 5. Extracción de los puntos (Borrar el suelo) ----
-    pcl::ExtractIndices<pcl::PointXYZI> extract;
-    extract.setInputCloud(cloud);
-    extract.setIndices(inliers);
-    extract.setNegative(true);  
-    extract.filter(*cloud_filtered);
-
-
-
-    // ---- 6. Guardado del resultado ----
-    pcl::io::savePCDFileASCII("conos_aislados.pcd", *cloud_filtered);
+    // Coeficientes: Ax + By + Cz + D = 0
+    float a = coefficients->values[0];
+    float b = coefficients->values[1];
+    float c = coefficients->values[2];
+    float d = coefficients->values[3];
+    float norm = std::sqrt(a*a + b*b + c*c);
     
-    return (0);
+    // FILTRADO FINAL
+    pcl::PointCloud<PointXYZIRT>::Ptr objetos(new pcl::PointCloud<PointXYZIRT>);
+
+    float h = 0.4f; 
+    float intensidad_suelo = 40.0f;
+
+    for (const auto& p : cloud->points)
+    {
+        float dist = (a*p.x + b*p.y + c*p.z + d) / norm;
+
+        // El punto debe estar por encima de la intensidad promedio del suelo y por encima del umbral del plano.
+        if (dist >= h && p.intensity > intensidad_suelo) 
+        {
+            objetos->push_back(p);
+        }
+    }
+
+    // 5. Guardar resultados
+    pcl::io::savePCDFileBinary("objetos.pcd", *objetos);
+
+    std::cout << "Original: " << cloud->size() << " | Objetos: " << objetos->size() << std::endl;
+    return 0;
 }
